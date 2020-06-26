a2eacae410f90fdf8df91b26cd800e84ecd8ddcb
suresh46offical <67456330+suresh46offical@users.noreply.github.com>
2020-06-26 12:40:30 +0530

    eureka server code

---
+++ orderserver/src/main/java/com/dbs/orderserver/OrderserverApplication.java
@@ -0,0 +1,16 @@
+package com.dbs.orderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class OrderserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserverApplication.class, args);
+	}
+
+}
